16299e17f2ef5c961955f4ae199b4e1c6b0c5d88
Aguirre Novaes <aguirre.novaes@gmail.com>
2023-05-23 18:37:17 -0300

    Estilização 90% (falta API)

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,7 @@
 const apiUrl = 'https://pokeapi.co/api/v2/{endpoint}/';
+// A API do PokeAPI, é uma API de apenas consumação, onde nenhuma autenticação é necessária para acessá-la.
+// Todos os recursos são completamente abertos e disponíveis para o público.
+// Desde que se moveram para um host estático, o limite de requisições foi removido por completo.
+// Porém é recomendado a limitar a frequência de requisições para reduzir os custos de hosteamento da API.
+
+const listarPoke = 'https://pokeapi.co/api/v2/pokemon/'
